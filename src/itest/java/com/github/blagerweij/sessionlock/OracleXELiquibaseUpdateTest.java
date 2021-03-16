0357ff92c6a6b09e273600e724c5a9875dcfd1d3
Barry Lagerweij <b.lagerweij@carepay.co.ke>
2021-03-16 09:00:24 +0100

    Fix timezone issue

--- src/itest/java/com/github/blagerweij/sessionlock/OracleXELiquibaseUpdateTest.java
+++ src/itest/java/com/github/blagerweij/sessionlock/OracleXELiquibaseUpdateTest.java
@@ -5,6 +5,11 @@ import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 class OracleXELiquibaseUpdateTest extends AbstractLiquibaseUpdateTest {
+  public OracleXELiquibaseUpdateTest() {
+    // Somehow Timezone UTC is not recognized in Github Actions
+    System.setProperty("oracle.jdbc.timezoneAsRegion", "false");
+  }
+
   @Container
   private static final OracleContainer ORACLE_DB_CONTAINER =
       new OracleContainer("oracleinanutshell/oracle-xe-11g");
